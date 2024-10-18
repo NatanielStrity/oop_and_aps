75555c1c242ff3ddcd83119a89279dd87262add4
NatanielStrity <146105801+NatanielStrity@users.noreply.github.com>
2024-10-18 13:37:36 +0000

    a

--- lab_1.py
+++ lab_1.py
@@ -3,25 +3,23 @@ class SJurnal:
     def __init__(self, student, sbj):
         self.sbj = sbj
         self.student = student
-    grade_list = [1, 5, 5, 4, 3, 2, 5]
+    grade_list = [3, 3, 3, 3, 3, 4, 5]
+
+
     def grade(self, grade):
         self.grade_list.append(grade)
+    
 
     def printer(self):
         print(self.student)
         print(self.sbj)
         print(self.grade_list)
 
+
     def final_grade(self):
-        final_grad_ = sum(self.grade_list) / len(self.grade_list)
-        print(final_grad_)
-    
-sjurnal = SJurnal('Петросян', 'ШедевроХимия')
-sjurnal.printer()
-sjurnal.final_grade()
+       final_grade = sum(self.grade_list)/len(self.grade_list)
+       print(final_grade)
 
-    
-
-
-
-    
+sj = SJurnal('Петя', 'Химия')
+sj.printer()
+sj.final_grade()
